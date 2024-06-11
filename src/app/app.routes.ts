939713d4796d897a8db81e626dc726b617f2c9dc
TLHPHEMAPALA <hiruhemapala@gmail.com>
2024-06-11 09:31:58 +0530

    login page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,23 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
 import { HomeComponent } from './home/home.component';
+import { MegamenuComponent } from './megamenu/megamenu.component';
+import { ClothingComponent } from './clothing/clothing.component';
+import { ShoesComponent } from './shoes/shoes.component';
+import { BlogComponent } from './blog/blog.component';
+
 
 export const routes: Routes = [
-    {path:'home',
-    component:HomeComponent,
-    },
+  { path: '', component: HomeComponent },
+  { path: 'megamenu', component: MegamenuComponent },
+  { path: 'clothing', component: ClothingComponent },
+  { path: 'shoes', component: ShoesComponent },
+  { path: 'blog', component: BlogComponent },
 ];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
---
+++ src/app/megamenu/megamenu.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-megamenu',
+  standalone: true,
+  imports: [],
+  templateUrl: './megamenu.component.html',
+  styleUrl: './megamenu.component.scss'
+})
+export class MegamenuComponent {
+
+}
