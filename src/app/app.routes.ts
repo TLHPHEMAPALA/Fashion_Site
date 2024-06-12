07c73c4ce855f8e85c033990b216b203bb1f4ea3
TLHPHEMAPALA <hiruhemapala@gmail.com>
2024-06-12 13:30:47 +0530

    updated home page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,6 +6,7 @@ import { MegamenuComponent } from './megamenu/megamenu.component';
 import { ClothingComponent } from './clothing/clothing.component';
 import { ShoesComponent } from './shoes/shoes.component';
 import { BlogComponent } from './blog/blog.component';
+import { LoginComponent } from './login/login.component';
 
 
 export const routes: Routes = [
@@ -14,6 +15,7 @@ export const routes: Routes = [
   { path: 'clothing', component: ClothingComponent },
   { path: 'shoes', component: ShoesComponent },
   { path: 'blog', component: BlogComponent },
+  {path:'explore',component:LoginComponent}
 ];
 
 @NgModule({
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet, RouterLink],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
-export class HomeComponent {}
+export class HomeComponent {
+  title='routing-app'
+}
